5a352079148a61f60d9a6f82b04a47b678264fec
Jasmine Wang <jwang131@u.rochester.edu>
2020-05-29 20:44:41 -0700

    work for first time

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const game = () => {
     let catX = 0;
     let catY = 0;
 
-    
+    let found = false;
 
     const clickScreen = document.querySelector('.click-screen');
     clickScreen.addEventListener("click", getClickPosition, true);
@@ -26,56 +26,41 @@ const game = () => {
     cat.style.marginLeft = `${catX}px`;
     cat.style.marginTop = `${catY}px`;
 
-    // const checkResult = () => {
-    //     if (mouseX >= catX && mounseX <= (catX + cat.width) && mouseY >=catY && mouseY <=(catY + cat.height))
-    //     {
-    //         alert("You found the cat!");
-    //         cat.classList.add('fadeIn');
-    //     }
-    //     else{
-    //         console.log("x:" + mouseX);
-    //         console.log("y:" + mouseY);
-    //         console.log("catX" + catX);
-    //         console.log("catY" + catY);
-    //         console.log("too bad");
-    //     }
-    // }
-
     function getClickPosition(e) {
         const mouseX = e.clientX;
         const mouseY = e.clientY;
         
-        console.log(mouseX);
-        console.log(mouseY);
+        if(found === true)
+        {
+            cat.toggleAttribute("fadeIn", false);
+            clickScreen.style.backgroundColor  = "#fafad2";
+            found = false;
+            game();
+        }
         
         if (mouseX >= catX && mouseX <= (catX + cat.width) && mouseY >=catY && mouseY <=(catY + cat.height))
         {
-            clickScreen.style.backgroundColor  = "#a1a112";
-            alert("You found the cat!");
             cat.classList.add('fadeIn');
+            clickScreen.style.backgroundColor  = "#a1a112";
+            found = true;
         }
-        else if ((catX - mouseX <= 50 || mouseX - (catX + cat.width) <= 50)
-                && (catY - mouseY <= 50 || mouseY - (catY + cat.height) <= 50)){
-                    console.log("hahh1");
-
+        else if (((catX-mouseX > 0 && catX - mouseX <= 150) || mouseX - (catX + cat.width) <= 150)
+                && ((catY - mouseY >0 && catY - mouseY <= 150) || mouseY - (catY + cat.height) <= 150)){
             clickScreen.style.backgroundColor  = "#e6e619";
         }
-        else if ((catX - mouseX <= 125 || mouseX - (catX + cat.width) <= 125)
-                && (catY - mouseY <= 125 || mouseY - (catY + cat.height) <= 125)){
-                    console.log("hahh4");
+        else if (((catX-mouseX > 0 && catX - mouseX <= 350) || mouseX - (catX + cat.width) <= 350)
+                && ((catY - mouseY >0 &&catY - mouseY <= 350) || mouseY - (catY + cat.height) <= 350)){
 
             clickScreen.style.backgroundColor  = "#eded5e";
         }
-        else if ((catX - mouseX <= 750 || mouseX - (catX + cat.width) <= 750)
-                && (catY - mouseY <= 750 || mouseY - (catY + cat.height) <= 750)){
-                    console.log("hahh7");
+        else if (((catX-mouseX > 0 && catX - mouseX <= 550) || mouseX - (catX + cat.width) <= 550)
+                && ((catY - mouseY >0 && catY - mouseY <= 550) || mouseY - (catY + cat.height) <= 550)){
 
             clickScreen.style.backgroundColor  = "#f2f28c";
         }
         else
         {
             clickScreen.style.backgroundColor  = "#f7f7ba";
-            console.log("hahh");
         }
 
     }
